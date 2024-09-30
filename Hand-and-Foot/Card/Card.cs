37cf5bca29c4c66d7fe27c91839e1cbcf1e6875e
Chase Hammond <u1211283@umail.utah.edu>
2024-09-30 13:55:49 -0600

    Implemented and tested functionality of Card class.

--- Hand-and-Foot/Card/Card.cs
+++ Hand-and-Foot/Card/Card.cs
@@ -1,7 +1,155 @@
-﻿namespace Card
-{
-    public class Card
-    {
+﻿namespace Card;
 
+public class Card
+{
+    private string Suit { get; set; }
+    private int CardValue { get; set; }
+
+    /// <summary>
+    /// Constructor for the card class.
+    /// </summary>
+    /// <param name="suit">The given suit of the card</param>
+    /// <param name="cardValue">The given value of the card 1-15</param>
+    public Card(int cardValue, string suit)
+    {
+        this.Suit = suit.ToLower();
+        this.CardValue = cardValue;
+    }
+
+    /// <summary>
+    /// Equals operater overload, based on card value.
+    /// </summary>
+    /// <param name="c1">An instance of the card class</param>
+    /// <param name="c2">An instance of the card class</param>
+    /// <returns>True if the cards are equal, false otherwise.</returns>
+    public static bool operator ==(Card c1, Card c2)
+    {
+        if (c1.CardValue.Equals(c2.CardValue))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Not equals operater overload, based on card value.
+    /// </summary>
+    /// <param name="c1">An instance of the card class</param>
+    /// <param name="c2">An instance of the card class</param>
+    /// <returns>True if the cards are not equal, false otherwise.</returns>
+    public static bool operator !=(Card c1, Card c2)
+    {
+        if (!c1.CardValue.Equals(c2.CardValue))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Lessthan operater overload, based on card value.
+    /// </summary>
+    /// <param name="c1">An instance of the card class</param>
+    /// <param name="c2">An instance of the card class</param>
+    /// <returns>True c1 is less than c2, false otherwise.</returns>
+    public static bool operator <(Card c1, Card c2)
+    {
+        if (c1.CardValue < c2.CardValue)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Greaterthan operater overload, based on card value.
+    /// </summary>
+    /// <param name="c1">An instance of the card class</param>
+    /// <param name="c2">An instance of the card class</param>
+    /// <returns>True c1 is greater than c2, false otherwise.</returns>
+    public static bool operator >(Card c1, Card c2)
+    {
+        if (c1.CardValue > c2.CardValue)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Gets the name of a card.
+    /// </summary>
+    /// <returns>A string containg the name of the card in the form "Name" of "Suit".</returns>
+    public string GetName()
+    {
+        string name = string.Empty;
+
+
+        if (CardValue == 11)
+        {
+            name = "Jack";
+        }
+        else if (CardValue == 12)
+        {
+            name = "Queen";
+        }
+        else if (CardValue == 13)
+        {
+            name = "King";
+        }
+        else if (CardValue == 14)
+        {
+            name = "Joker";
+        }
+        else if (CardValue == 1)
+        {
+            name = "Ace";
+        }
+        else
+        {
+            name = CardValue.ToString();
+        }
+
+        return $"{name} of {Suit}";
+    }
+
+    /// <summary>
+    /// Gets the point value of a card.
+    /// </summary>
+    /// <returns>The point value of a card.</returns>
+    public int GetPointValue()
+    {
+        if (CardValue == 2)
+        {
+            return 25;
+        }
+        else if (CardValue == 3 && (Suit == "spades" || Suit == "clubs"))
+        {
+            return 300;
+        }
+        else if (CardValue == 3 && (Suit == "hearts" || Suit == "diamonds"))
+        {
+            return 150;
+        }
+        else if (CardValue > 8)
+        {
+            return 10;
+        }
+        else
+        {
+            return 5;
+        }
     }
 }
--- Hand-and-Foot/CardTest/CardTest.cs
+++ Hand-and-Foot/CardTest/CardTest.cs
@@ -13,17 +13,83 @@ public class CardTest
     {
         Card card = new Card(11,"Hearts");
 
-        Assert.AreEqual("Jack of Hearts", card.GetName());
+        Assert.AreEqual("Jack of hearts", card.GetName());
     }
 
     /// <summary>
-    /// Creates a new card with 11, and "Heards" and checks that the vale is correct.
+    /// Creates a new card with 11, and "Hearts" and checks that the vale is correct.
     /// </summary>
     [TestMethod]
     public void Card_CreateCardValue_True()
     {
-        Card card = new Card(11, "Hearts");
+        Card card = new Card(11, "hearts");
 
-        Assert.AreEqual(10)
+        Assert.AreEqual(10, card.GetPointValue());
+    }
+
+    /// <summary>
+    /// Creates a new card with 3, and "Hearts" and checks that the vale is correct.
+    /// </summary>
+    [TestMethod]
+    public void Card_CheckCardValueRed3_True()
+    {
+        Card card = new Card(3, "hearts");
+
+        Assert.AreEqual(150, card.GetPointValue());
+    }
+
+    /// <summary>
+    /// Creates a new card with 3, and "spades" and checks that the vale is correct.
+    /// </summary>
+    [TestMethod]
+    public void Card_CheckCardValueBlack3_True()
+    {
+        Card card = new Card(3, "spades");
+
+        Assert.AreEqual(300, card.GetPointValue());
+    }
+
+    /// <summary>
+    /// Tests that the equals overload for the card class properly checks equality based on the card
+    /// </summary>
+    [TestMethod]
+    public void Card_EqualsOverload_True()
+    {
+        Card card1 = new Card(11, "Hearts");
+        Card card2 = new Card(11, "Hearts");
+        Assert.IsTrue(card1 == card2);
+    }
+
+    /// <summary>
+    /// Tests that the notequals overload for the card class properly checks equality based on the card
+    /// </summary>
+    [TestMethod]
+    public void Card_NotEqualsOverload_True()
+    {
+        Card card1 = new Card(11, "Hearts");
+        Card card2 = new Card(10, "Hearts");
+        Assert.IsTrue(card1 != card2);
+    }
+
+    /// <summary>
+    /// Tests that the greaterthan overload for the card class properly checks equality based on the card
+    /// </summary>
+    [TestMethod]
+    public void Card_GreaterthanOverload_True()
+    {
+        Card card1 = new Card(11, "Hearts");
+        Card card2 = new Card(10, "Hearts");
+        Assert.IsTrue(card1 > card2);
+    }
+
+    /// <summary>
+    /// Tests that the lessthan overload for the card class properly checks equality based on the card
+    /// </summary>
+    [TestMethod]
+    public void Card_LessthanOverload_True()
+    {
+        Card card1 = new Card(11, "Hearts");
+        Card card2 = new Card(10, "Hearts");
+        Assert.IsTrue(card2 < card1);
     }
 }
